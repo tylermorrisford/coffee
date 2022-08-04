7528edca02e4450cb89e160f740ea6b89a119f25
Tyler Ford <tyler.ford@commercetools.com>
2022-08-03 22:10:08 -0400

    update app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
         <header className="App-header">
-          <p> I like coffee</p>
+          <p>Hi, I'm tyler</p>
+          <p>I like coffee</p>
           <Emoji emoji="hot-beverage"/>
         </header>
     </div>
